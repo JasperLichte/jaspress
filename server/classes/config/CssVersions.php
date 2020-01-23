81760ceaf029c0797c8ca7853e13a320fa53d9a2
Jasper Lichte <jlichte@hh.rexx-systems.rx>
2020-01-23 15:38:31 +0100

    Remove old css files, get rid of helpers

--- server/classes/config/CssVersions.php
+++ server/classes/config/CssVersions.php
@@ -8,9 +8,5 @@
     {
         public const VERSIONS = [
             'document.css' => 1,
-            '/components/footer.css' => 1,
-            '/components/header.css' => 1,
-            '/components/main.css' => 1,
-            '/pages/start.css' => 1,
-            ];
+        ];
     }
