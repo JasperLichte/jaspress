4714b5ad46f2a74cc9a85910d72047ce5738a3bf
JasperLichte <contact@jasper-lichte.com>
2020-03-01 11:21:31 +0100

    Add parsedown #2

--- server/autoload.php
+++ server/autoload.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once(__DIR__ . './vendor/autoload.php');
+
 function autoloader($class)
 {
     $filename = realpath(dirname(__FILE__)) . '/classes/' . str_replace('\\', '/', $class) . '.php';
